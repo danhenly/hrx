ba7f7a4d192bd9b4acd48d68b372ecb6e91add09
dan <danhenlysales@gmail.com>
2026-03-09 15:02:20 +0800

    Remove FloatingButton component from Layout for cleaner structure

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,23 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>{children}</SidebarInset>
-      <FloatingButton />
     </SidebarProvider>
   );
 }
 
-function FloatingButton() {
-  return (
-    <div className="fixed right-2 bottom-2 z-50">
-      <Button size="sm" asChild>
-        <a
-          href="https://github.com/openstatusHQ/openstatus-template"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub Repo
-        </a>
-      </Button>
-    </div>
-  );
-}
